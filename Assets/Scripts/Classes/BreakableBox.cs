d2e15505077494a232d7502b8e7f6a33660555af
kobi3455 <jakub.kobezda@gmail.com>
2026-04-17 21:23:58 +0200

    wall bounce

--- Assets/Scripts/Classes/BreakableBox.cs
+++ Assets/Scripts/Classes/BreakableBox.cs
@@ -3,20 +3,20 @@ public enum BoxType { Wood, Steel }
 public class BreakableBox : WorldObject, ISpawnable
 {
     public BoxType type;
-    public MonoBehaviour containedItem; // must implement ICollectible
+    // public MonoBehaviour containedItem; // must implement ICollectible
 
     public void OnSpawn()
     {
         // spawn logic
     }
 
-    public void Break(PlayerController player)
-    {
-        if (containedItem is ICollectible collectible)
-        {
-            collectible.OnCollect(player);
-        }
+    // public void Break(PlayerController player)
+    // {
+    //     if (containedItem is ICollectible collectible)
+    //     {
+    //         collectible.OnCollect(player);
+    //     }
 
-        Destroy(gameObject);
-    }
+    //     Destroy(gameObject);
+    // }
 }
